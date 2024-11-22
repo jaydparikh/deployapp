2a0fb2493a97b11a89e43433c3c88eee8eeb19d2
Jay Parikh <jayparikh265@gmail.com>
2024-11-22 19:53:34 +0530

    corrected password

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from models import Base
 
 DATABASE_URL = "sqlite:///database.db"
 
-engine = create_engine('mysql+pymysql://jayparikh:Fyndtest@123@jayparikh.mysql.pythonanywhere-services.com/jayparikh$fynd')
+engine = create_engine('mysql+pymysql://jayparikh:Fyndtest123@jayparikh.mysql.pythonanywhere-services.com/jayparikh$fynd')
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
